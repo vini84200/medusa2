f945f0bb3e4ce12c4c7b219ac023ff32b458edac
Vini84200 <www.vini84200@hotmail.com>
2019-01-22 17:45:05 -0200

    Preconfiguração para adicionar sistema de websocket para atualizações
    Signed-off-by: Vini84200 <www.vini84200@hotmail.com>

--- MedusaII/settings.py
+++ MedusaII/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'MedusaII.urls'
-
+ASGI_APPLICATION = "MedusaII.routing.application"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -69,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'MedusaII.wsgi.application'
+# WSGI_APPLICATION = 'MedusaII.wsgi.application'
 
 
 # Database
---
+++ voting/consumers.py
@@ -0,0 +1,15 @@
+import asyncio
+import json
+from django.contrib.auth import get_user_model
+from channels.consumer import AsyncConsumer
+from channels.db import database_sync_to_async
+
+class PainelPresencaConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        print("connected", event)
+
+    async def websocket_receive(self, event):
+        print("Received ", event)
+
+    async def websocket_disconnect(self, event):
+        print("Disconnected ", event)
