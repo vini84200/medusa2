7aa8f55b5389ec5456d8e3c8bcf2db78c3a9eb8f
Vini84200 <www.vini84200@hotmail.com>
2019-03-15 14:02:26 -0300

    Criado alguns testes, arquivo .travis.yml
    Signed-off-by: Vini84200 <www.vini84200@hotmail.com>

--- escola/tests/test_models.py
+++ escola/tests/test_models.py
@@ -2,5 +2,3 @@ import pytest
 from mixer.backend.django import mixer
 
 pytestmark = pytest.mark.django_db
-
-class Test
--- escola/tests/test_views.py
+++ escola/tests/test_views.py
@@ -1,8 +1,14 @@
+from django.http import HttpResponseRedirect
+from django.test.client import Client
 import pytest
 from mixer.backend.django import mixer
+from django.urls import reverse
 
 pytestmark = pytest.mark.django_db
 
-class TestIndex():
-    def test_annonymous(self, cl, rf):
-        #TODO Continuar testes;
+
+class TestIndex:
+    def test_annonymous(self, client: Client, rf):
+        response = client.get(reverse('escola:index'), follow=False)
+        assert response.status_code == 302, 'Caso um usario não logue deve ser redirecionado'
+        assert reverse('login') in response.url
