c2f4185218583df3f4a080c33c6cb3866dc3ddc0
Vinicius J. F <www.vini84200@hotmail.com>
2019-04-18 18:10:04 -0300

    Update apps.py

--- escola/apps.py
+++ escola/apps.py
@@ -8,7 +8,3 @@ from django.apps import AppConfig
 class EscolaConfig(AppConfig):
     name = 'escola'
 
-    def ready(self):
-        from django.db.models.signals import post_migrate
-        from .signals import populate_models
-        post_migrate.connect(populate_models, sender=self)
