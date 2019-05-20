45773e6635dff8b1535adfb144d83524c64d627c
Vini84200 <www.vini84200@hotmail.com>
2019-05-20 15:15:43 -0300

    Criado Esqueleto das views

--- escola/views_provas_marcadas.py
+++ escola/views_provas_marcadas.py
@@ -1,4 +1,26 @@
 #  Developed by Vinicius José Fritzen
 #  Last Modified 20/05/19 15:02.
 #  Copyright (c) 2019  Vinicius José Fritzen and Albert Angel Lanzarini
+import logging
 
+logger = logging.getLogger(__name__)
+
+# Lista de Provas da Turma
+
+# Adicionar prova de materia
+
+# Adicionar prova de area
+
+# Editar Prova de Materia
+
+# Editar Prova de Area
+
+# Apagar Prova de Materia
+
+# Apagar prova de area
+
+# Detalhes de prova
+
+# Adicionar conteudos a prova
+
+# Datas Livres da turma
