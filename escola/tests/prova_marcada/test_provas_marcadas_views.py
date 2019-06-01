15199348ef1eb0df76b505d02f0e920690ce1ef8
Vini84200 <www.vini84200@hotmail.com>
2019-06-01 19:55:30 -0300

    Adicionado ultimos testes das provas marcadas

--- escola/tests/prova_marcada/test_provas_marcadas_views.py
+++ escola/tests/prova_marcada/test_provas_marcadas_views.py
@@ -42,3 +42,40 @@ def test_list_provas_professor(professor_client: Client, tc: TestCase):
     response = professor_client.get(reverse('escola:provas-professor'))
     tc.assertContains(response, "<h1>Minhas Provas marcadas</h1>")
     tc.assertTemplateUsed(response, 'escola/provas_marcadas/listProvasProfessor.html')
+
+
+@pytest.mark.provas_marcadas
+def test_delete_prova_materia_get(prova_marcada_materia, tc: TestCase, client: Client):
+    client.force_login(prova_marcada_materia.get_owner())
+    response = client.get(reverse('escola:provas-materia-delete', args=[prova_marcada_materia.pk]))
+    tc.assertTemplateUsed(response, 'escola/base_delete.html')
+
+
+@pytest.mark.provas_marcadas
+def test_delete_prova_area_get(prova_marcada_area, tc: TestCase, client: Client):
+    client.force_login(prova_marcada_area.get_owner())
+    response = client.get(reverse('escola:provas-area-delete', args=[prova_marcada_area.pk]))
+    tc.assertTemplateUsed(response, 'escola/base_delete.html')
+
+
+@pytest.mark.provas_marcadas
+def test_detail_prova_materia_get(prova_marcada_materia, tc: TestCase, aluno_client: Client):
+    response = aluno_client.get(prova_marcada_materia.get_absolute_url())
+    tc.assertContains(response, prova_marcada_materia.get_nome())
+    tc.assertContains(response, prova_marcada_materia.get_descricao())
+    tc.assertContains(response, prova_marcada_materia.get_apresentacao())
+    tc.assertTemplateUsed(response, 'escola/provas_marcadas/detail_prova.html')
+
+
+@pytest.mark.provas_marcadas
+def test_detail_prova_area_get(prova_marcada_area, tc: TestCase, aluno_client: Client):
+    response = aluno_client.get(prova_marcada_area.get_absolute_url())
+    tc.assertContains(response, prova_marcada_area.get_nome())
+    tc.assertContains(response, prova_marcada_area.get_descricao())
+    tc.assertTemplateUsed(response, 'escola/provas_marcadas/detail_prova.html')
+
+
+@pytest.mark.provas_marcadas
+def test_calendario_turma_get(turma: Turma, tc: TestCase, professor_client: Client):
+    response = professor_client.get(reverse('escola:turma-provas-calendario', args=[turma.pk, ]))
+    tc.assertTemplateUsed(response, 'escola/provas_marcadas/calendarioDatasLivres.html')
