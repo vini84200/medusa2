0e368b14c44b830777ff735feac83c13a0e04115
Vini84200 <www.vini84200@hotmail.com>
2019-06-30 12:00:30 -0300

    Corrigido problema da pagina de alterar email

--- escola/views_profile.py
+++ escola/views_profile.py
@@ -6,14 +6,12 @@ import datetime
 
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, render_to_response
 from django.template import RequestContext
 from django.views.generic import ListView, TemplateView
 
 from escola.models import Horario, Turno, TurnoAula
-from .decorators import *
-from .forms import *
-from .models import *
 
 logger = logging.getLogger(__name__)
 
@@ -25,6 +23,6 @@ def email_change(request):
         form = EmailChangeForm(request.user, request.POST)
         if form.is_valid():
             form.save()
-            return reverse('escola:index')
+            return HttpResponseRedirect(reverse('escola:index'))
 
     return render(request, "escola/email_change.html", {'form': form})
