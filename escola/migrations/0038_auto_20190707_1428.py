d248ff7de3522fb287cdc53ca2eefadce92f5944
Vini84200 <www.vini84200@hotmail.com>
2019-07-07 22:54:46 -0300

    Corrigido a migration

--- escola/migrations/0038_auto_20190707_1428.py
+++ escola/migrations/0038_auto_20190707_1428.py
@@ -6,7 +6,7 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('escola', '0037_profile_receber_email_notificacao'),
