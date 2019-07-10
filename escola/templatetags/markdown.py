d1b2f35c1302de21afc96876ad001394cbb10e90
Vini84200 <www.vini84200@hotmail.com>
2019-07-10 09:29:13 -0300

    Adicionado tag para exibir markdown

--- escola/templatetags/markdown.py
+++ escola/templatetags/markdown.py
@@ -1,13 +1,15 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 import misaka as m
 
 register = template.Library()
 
+
 @register.filter(is_safe=True)
 @stringfilter
-def gfm(value):
-    rendered_text = m.html(value,
-                           extensions=m.EXT_FENCED_CODE,
-                           render_flags=m.HTML_ESCAPE)
+def md(value):
+    rendered_text = mark_safe(m.html(value,
+                              extensions=m.EXT_FENCED_CODE,
+                              render_flags=m.HTML_ESCAPE))
     return rendered_text
